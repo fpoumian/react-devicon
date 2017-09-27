7b9a4426b49e895a1d1e6e9cac9691d1a135bb3c
Fernando Poumián <fpoumian@gmail.com>
2017-09-27 11:36:03 -0600

    Add components index file generation.

--- scripts/generateComponents.js
+++ scripts/generateComponents.js
@@ -1,57 +1,44 @@
-
-const fs = require('fs-extra')
-const path = require('path')
-const paths = require('../config/paths')
-const nunjucks = require('nunjucks')
-const judex = require('judex-component-generator')
-const pascalCase = require('pascal-case')
-const { getDeviconPath, getDeviconManifestFile } = require('./devicon')
-const SVGO = require('svgo')
+const fs = require("fs-extra")
+const path = require("path")
+const paths = require("../config/paths")
+const nunjucks = require("nunjucks")
+const judex = require("judex-component-generator")
+const pascalCase = require("pascal-case")
+const { getDeviconPath, getDeviconManifestFile } = require("./devicon")
+const SVGO = require("svgo")
 
 // global
 let deviconPath
+let indexFileWriteStream
 const svgo = new SVGO()
-
-function optimizeSVG(data) {
-  return new Promise((resolve, reject) => {
-    svgo.optimize(data, (result, err) => {
-      if (err) {
-        console.error(err)
-        reject(err)
-      }
-      console.log(result)
-      resolve(result)
-    })
-  })
-}
+const componentsIndex = path.resolve(paths.components, "index.js")
 
 // Create a Judex generator
 const generator = judex({
   paths: {
     components: paths.components,
-    templates: path.resolve(__dirname, 'templates')
+    templates: path.resolve(__dirname, "templates")
   },
   rules: {
-    'component-name-root-dir': false
+    "component-name-root-dir": false
   }
 })
 
 function generateIconComponent(iconName, iconVersion) {
   const componentName = `${pascalCase(iconName)}${pascalCase(iconVersion)}`
   const svgFileName = `${iconName}-${iconVersion}.svg`
-  const svgFilePath = path.resolve(deviconPath, 'icons', iconName, svgFileName)
+  const svgFilePath = path.resolve(deviconPath, "icons", iconName, svgFileName)
 
   return new Promise((resolve, reject) => {
     generator
       .generate(`${iconName}/${iconVersion}/${componentName}`, {
         index: true
       })
-      .on('done', componentPaths => {
+      .on("done", componentPaths => {
         fs
-          .readFile(svgFilePath, 'utf8')
+          .readFile(svgFilePath, "utf8")
           // .then(svg => optimizeSVG(svg))
           .then(data => {
-            console.log(componentPaths.root)
             return fs.writeFile(
               path.resolve(componentPaths.root, `${componentName}.svg`),
               data
@@ -65,16 +52,32 @@ function generateIconComponent(iconName, iconVersion) {
               paths.examples
             )
           )
+          .then(() => {
+            return writeExportToStream(
+              indexFileWriteStream,
+              componentName,
+              iconName,
+              iconVersion
+            )
+          })
           .then(() => fs.readdir(componentPaths.root))
           .then(dirContents => resolve(dirContents))
-          .catch(console.error)
       })
-      .on('error', error => {
+      .on("error", error => {
         reject(error)
       })
   })
 }
 
+function writeExportToStream(
+  writeStream,
+  componentName,
+  iconName,
+  iconVersion
+) {
+  const templateString = `export ${componentName} from './${iconName}/${iconVersion}'\n`
+  writeStream.write(templateString)
+}
 function generateComponentExamples(
   componentName,
   iconName,
@@ -83,14 +86,14 @@ function generateComponentExamples(
 ) {
   const context = {
     defaultFunctionIdentifier: `${componentName}Example`,
-    defaultImportIdentifier: pascalCase(iconName) + 'Icon',
-    width: '100px',
-    height: '100px',
+    defaultImportIdentifier: pascalCase(iconName) + "Icon",
+    width: "100px",
+    height: "100px",
     iconVersion,
     iconName
   }
 
-  const templateString = require('./templates/icon-component-example')
+  const templateString = require("./templates/icon-component-example")
   const template = nunjucks.compile(templateString)
   const data = template.render(Object.assign({}, context))
 
@@ -109,6 +112,17 @@ function generateComponents(iconsArray) {
   return Promise.all(promisesArray.map(promiseSet => Promise.all(promiseSet)))
 }
 
+function optimizeSVG(data) {
+  return new Promise((resolve, reject) => {
+    svgo.optimize(data, (result, err) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(result)
+    })
+  })
+}
+
 // Check if devicon package is installed locally
 // If not, abort the script
 try {
@@ -121,11 +135,19 @@ try {
   process.exit(e.code)
 }
 
-getDeviconManifestFile()
-// parse devicon.json file into object
+fs
+  .outputFile(componentsIndex, "")
+  .then(() => {
+    return (indexFileWriteStream = fs.createWriteStream(componentsIndex, {
+      flags: "a"
+    }))
+  })
+  .then(() => getDeviconManifestFile())
   .then(fs.readJson)
   .then(generateComponents)
-  .then(console.log)
+  .then(() => {
+    return indexFileWriteStream.end()
+  })
   .catch(err => {
     process.exit(err.code)
   })
